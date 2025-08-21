a186fbdc35fc4c20c723e32bd29433fbe2b0b4aa
mohdrashadi-pro <moalrashadi@gmail.com>
2025-08-21 18:57:03 +0800

    -m new update

--- oob/lesson/1.cpp
+++ oob/lesson/1.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std; 
 
+
+// Definition 
 class car {
+private: 
+bool isExpired() {
+int currentYear = 2025;
+return (currentYear -  year ) > 15 ;
+
+
+}
+
+
+
 public: 
 // Properties 
 string brands;
 string model;
 int year;
-
 
 // Methode 
 
@@ -15,32 +26,37 @@ void displayinfo() {
 cout << "Brands:" <<brands<< endl;
 cout << "model: " <<model<< endl;
 cout << "year: " <<year << endl;
-
-
-
+cout << "Expired "  << (isExpired () ? "Yes " : "No") << endl;
 }
-
-
-
 };
 
 int main () {
-
-
-
     //object 
-
 car car1;
 car car2;
+cout << "Enter the the name of first car: ";
+cin >>car1.brands;  
 
-car1.brands = "Niasan  ";
-car2.brands = "BMW   ";
+cout << "Enter the the name of second car: ";
+cin >>car2.brands; 
 
-car1.model = "sunnay   ";
-car2.model = "normal    ";
 
-car1.year = 2001;
-car2.year = 2003;
+
+
+ 
+
+
+cout << "Enter the the name of modul car: ";
+cin >>car1.model;
+cout << "Enter the the name of modul  car: ";
+cin >>car2.model; 
+
+cout << "Enter the the name of year car: ";
+cin >>car1.year;
+cout << "Enter the the name of year  car: ";
+cin >>car2.year;
+
+
 
 
 car1.displayinfo() ;
