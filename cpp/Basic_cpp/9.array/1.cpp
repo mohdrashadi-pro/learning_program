a4a3bf3e1bb6f43e89601ce2b28b78bc526043ee
mohdrashadi-pro <moalrashadi@gmail.com>
2025-08-25 14:01:44 +0800

    change name from 'oob' to 'oop'

---
+++ cpp/Basic_cpp/9.array/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int grades[5] = {54, 78, 98, 45, 34} ;
+cout << grades[0] << endl;
+
+ grades[0] = 23;
+cout << grades[0];
+
+    return 0;
+}
